07213b988f93a92963f88bd898d71b60d01fa77d
MXoneDay <markmolenvd@gmail.com>
2021-11-23 16:45:06 +0100

    updateing lauch file

--- app_verpleegkunde/lib/screens/login/main.dart
+++ app_verpleegkunde/lib/screens/login/main.dart
@@ -9,20 +9,23 @@ import '../second.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
+// OUT OF APP SCOPE ESTABLISHING CONNECTION TO DB FOR LOGIN
 Future<Album> fetchAlbum(title, context) async {
-  final response = await http
-      .post(Uri.parse('https://iabamun.nl/game/lab-andre/api/index.php/login'),
-      body: jsonEncode(<String, String>{
-        "name": title.toString(),
-        "password": "KoekjesZijnGemaaktVanDeeg",
-      }),
-    );
+  final response = await http.post(
+    Uri.parse('https://iabamun.nl/game/lab-andre/api/index.php/login'),
+    body: jsonEncode(<String, String>{
+      "name": title.toString(),
+      "password": "KoekjesZijnGemaaktVanDeeg",
+    }),
+  );
   var data = jsonDecode(response.body);
 
-  if(data['response'] != null){
-    if(data['response'] == "Logged in"){
-      Navigator.push(context, 
-      MaterialPageRoute(builder: (context) => const MyHomePage(title: 'wat')));
+  if (data['response'] != null) {
+    if (data['response'] == "Logged in") {
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => const MyHomePage(title: 'wat')));
     }
   }
 
@@ -36,7 +39,7 @@ class Album {
     required this.response,
   });
 
-  getResponse(){
+  getResponse() {
     return response;
   }
 
@@ -69,25 +72,22 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Hanze Verpleeg App',
-      theme: ThemeData(
-        scaffoldBackgroundColor: const Color(0xFFe3e6e8),
-        primarySwatch: Colors.blue,
-      ),
-      home: Builder(
-        builder: (context) =>Scaffold(
-          appBar: AppBar(
-            title: const Text('Nurse - IT'),
-          ),
-          body: 
-          Center(
-            child: buildColumn(context)
-          ),
+        title: 'Hanze Verpleeg App',
+        theme: ThemeData(
+          scaffoldBackgroundColor: const Color(0xFFe3e6e8),
+          primarySwatch: Colors.blue,
         ),
-      )
-    );
+        home: Builder(
+          builder: (context) => Scaffold(
+            appBar: AppBar(
+              title: const Text('Nurse - IT'),
+            ),
+            body: Center(child: buildColumn(context)),
+          ),
+        ));
   }
-  Table buildColumn(BuildContext context){
+
+  Table buildColumn(BuildContext context) {
     final myController = TextEditingController();
 
     @override
@@ -96,41 +96,44 @@ class _MyAppState extends State<MyApp> {
       myController.dispose();
       super.dispose();
     }
-    return Table(
-      columnWidths:{ 
-        0: FlexColumnWidth(8),
-        1: FlexColumnWidth(6)
-      },
-      children: [
-        TableRow(children: [ Text(error, style:TextStyle(color: Colors.red, fontWeight: FontWeight.bold)),Text("")]
+
+    return Table(columnWidths: {
+      0: FlexColumnWidth(8),
+      1: FlexColumnWidth(6)
+    }, children: [
+      TableRow(children: [
+        Text(error,
+            style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold)),
+        Text("")
+      ]),
+      TableRow(children: [
+        TextField(
+          decoration: InputDecoration(fillColor: Colors.white, filled: true),
+          controller: myController,
         ),
-        TableRow(
-         children:[TextField(
-            decoration: InputDecoration( fillColor: Colors.white, filled: true),
-            controller: myController,
+        Container(
+            child: TextButton(
+          onPressed: () {
+            futureAlbum = fetchAlbum(myController.text, context);
+            setState(() {
+              error = "Failed to login";
+            });
+          },
+          style: TextButton.styleFrom(
+              padding: EdgeInsets.all(25), backgroundColor: Colors.blue),
+          child: const Text(
+            "Login",
+            style: TextStyle(color: Colors.black),
           ),
-          Container(
-          
-            child: 
-            TextButton(
-              
-              onPressed: (){
-                futureAlbum = fetchAlbum(myController.text, context);
-                setState(() {
-                  error = "Failed to login";
-                });
-              },
-              style: TextButton.styleFrom(padding:EdgeInsets.all(25), backgroundColor: Colors.blue),
-              child: const Text("Login", style: TextStyle(color: Colors.black),),
-            )
-          ),
-        ]
-        )
-      ]
-    );
+        )),
+      ])
+    ]);
   }
-  login(BuildContext context){
-    Navigator.push(context, 
-    MaterialPageRoute(builder: (context) => const MyHomePage(title: 'wat')));
+
+  login(BuildContext context) {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => const MyHomePage(title: 'wat')));
   }
 }
