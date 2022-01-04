41ed71c316ed31bd5f3ec3a7b7f2a5dab97a5989
MXoneDay <markmolenvd@gmail.com>
2022-01-04 17:08:25 +0100

    relocate & take base styling of learninggoal

--- app_verpleegkunde/lib/screens/overview/overview.dart
+++ app_verpleegkunde/lib/screens/overview/overview.dart
@@ -1,14 +1,70 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/material.dart';
 
-class Overview extends StatefulWidget {
-  const Overview({Key? key}) : super(key: key);
+// ignore: camel_case_types
+class learningGoalOverview extends StatefulWidget {
+  // Iets voor de routes maar wat?
+  const learningGoalOverview({Key? key}) : super(key: key);
 
   @override
-  _OverviewState createState() => _OverviewState();
+  learningGoalOverviewState createState() => learningGoalOverviewState();
 }
 
-class _OverviewState extends State<Overview> {
+// ignore: camel_case_types
+class learningGoalOverviewState extends State<learningGoalOverview> {
+  List<Widget> generatedBody = [];
+  bool justOnce = false;
+
   @override
+  Widget build(BuildContext context) {
+    getTags() async {
+      final prefs = await SharedPreferences.getInstance();
+      List<String>? leerdoelen = prefs.getStringList('leerdoel');
+      return leerdoelen;
+      // return "\"leerdoel\":[{\"begin_datum\": \"23-01-2021\", \"eind_datum\": \"24-01-2021\", \"onderwerp\": \"leerdoel1\"},{\"begin_datum\": \"23-01-2021\", \"eind_datum\": \"24-01-2021\", \"onderwerp\": \"leerdoel1\"},{\"begin_datum\": \"23-01-2021\", \"eind_datum\": \"24-01-2021\", \"onderwerp\": \"leerdoel1\"},{\"begin_datum\": \"23-01-2021\", \"eind_datum\": \"24-01-2021\", \"onderwerp\": \"leerdoel1\"},]";
+    }
+
+    fillBody() async {
+      List<String>? leerdoelen = await getTags();
+      List<Widget> listToReturn = [];
+
+      if (leerdoelen == null) {
+        return;
+      }
+
+      for (String vari in leerdoelen) {
+        Map<String, dynamic> decodedLearningGoals = jsonDecode(vari);
+        print(decodedLearningGoals);
+        listToReturn.add(Row(
+          children: [
+            selectPeriod(
+                context,
+                decodedLearningGoals["begin_datum"],
+                decodedLearningGoals["eind_datum"],
+                decodedLearningGoals["onderwerp"])
+          ],
+        ));
+      }
+      setState(() {
+        generatedBody = listToReturn;
+      });
+    }
+
+    if (!justOnce) {
+      print("wat");
+      justOnce = true;
+      fillBody();
+    }
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Overzicht van leerdoelen"),
+          backgroundColor: Colors.orange,
+          centerTitle: true,
+        ),
+        body: Column(children: generatedBody));
+  }
 
   //TEST
   BoxDecoration borderStyling() {
@@ -21,17 +77,38 @@ class _OverviewState extends State<Overview> {
     );
   }
 
-  @override
-  // Widget that builds the scaffold that holds all the widgets that this screen consists off
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Overzicht van Leerdoelen"),
-        backgroundColor: Colors.orange,
-        centerTitle: true,
-      ),
-      // Body of the application
-      body: null,
-    );
+  //Widget for selecting a period in which that learning goal will be set
+  Widget selectPeriod(
+      BuildContext context, String startDate, String endDate, String testie) {
+    return Container(
+        margin: const EdgeInsets.only(left: 40.0, right: 40.0),
+        width: 300,
+        padding: const EdgeInsets.all(10.0),
+        decoration: borderStyling(),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+          Text(
+            testie,
+            style: const TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+          ),
+          // Creates spacing between items inside of the column
+          const SizedBox(
+            height: 8,
+          ),
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+            Column(
+              children: <Widget>[
+                Text(startDate),
+                const SizedBox(
+                  width: 20,
+                ),
+                Text(endDate),
+              ],
+            )
+          ])
+        ]));
   }
 }
