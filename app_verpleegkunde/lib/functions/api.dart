0c04fb297439f2d9f48836a8ae2ac5633b01c27f
Lelouch557 <andreblok@hotmail.nl>
2022-01-13 10:18:16 +0100

    try to implement subscription key

--- app_verpleegkunde/lib/functions/api.dart
+++ app_verpleegkunde/lib/functions/api.dart
@@ -24,33 +24,50 @@ class Response {
 // comment out line 32
 class Api {
   Future<bool> login(id, password) async {
-    var privateApi = "https://iabamun.nl/game/lab-andre/api/index.php/login";
-    var groupApi = "https://nurse-it.azurewebsites.net/api/test_login?name=$id&password=$password";
+    // var privateApi = "https://iabamun.nl/game/lab-andre/api/index.php/login";
+    // final response = await http.post(
+    //   Uri.parse(privateApi),
+    //   body: jsonEncode(<String, String>{"name": id, "password": password, }),
+    // );
+    print("trying");
+    
+    var groupApi = "https://nurse-it-api.azure-api.net/Nurse-IT/Login?name=user&password=KoekjesZijnGemaaktVanDeeg";
     final response = await http.post(
-      Uri.parse(privateApi),
-      body: jsonEncode(<String, String>{"name": id, "password": password, }),
+      Uri.parse(groupApi),
+    //   headers:{
+    //   "Host": "flutterVerpleegApp",
+    //   "Content-Length": "0",
+    //   "Ocp-Apim-Subscription-Key": "Ocp-Apim-Subscription-Key",
+    //   "Accept": "application/json",
+    //   "Access-Control-Allow-Origin": "*",
+    //  },
+      body:{}
     );
-    var data = jsonDecode(response.body);
-
-    if (data['response'] != null) {
-      if (data['response'] == "Logged in") {
-        return true;
-      }
-    }
+    print(response);
+    // var data = jsonDecode(response.body);
+    // print(data);
+    // if (data['response'] != null) {
+    //   if (data['response'] == "Logged in") {
+    //     return true;
+    //   }
+    // }
     return false;
   }
 
   Future<bool> syncUp(user_name, password, reflectie_json, leerdoel_json, week_reflectie_json) async{
     var testApi = "test";
     var groupApi = "https://nurse-it.azurewebsites.net/api/test_login?name=$user_name&password=$password&reflectie=$reflectie_json&leerdoel=$leerdoel_json&week_reflectie=$week_reflectie_json";
-    final response = await http.post(
-      Uri.parse(testApi)
-    );
+
+    // final response = await http.post(
+    //   Uri.parse(testApi)
+    // );
     
-    if(response.statusCode == 200){
-      return true;
-    }else{
-      return false;
-    }
+    // if(response.statusCode == 200){
+    //   return true;
+    // }else{
+    //   return false;
+    // }
+    print(groupApi);
+    return true;
   }
 }
--- app_verpleegkunde/lib/functions/syncronisatie.dart
+++ app_verpleegkunde/lib/functions/syncronisatie.dart
@@ -14,12 +14,16 @@ class Syncronisation {
 
     var name =  prefs.getString('user');
     var password = prefs.getString('password');
-    var reflectie_json = jsonEncode(prefs.getStringList('daily_reflection') ?? []);
-    var leerdoel_json = jsonEncode(prefs.getStringList('leerdoel') ?? []);
-    var week_reflectie_json = jsonEncode(prefs.getStringList('week_reflectie') ?? []);
+    var reflectie_json = prefs.getStringList('daily_reflection') ?? [];
+    var leerdoel_json = prefs.getStringList('leerdoel') ?? [];
+    var week_reflectie_json = prefs.getStringList('week_reflectie') ?? [];
 
+    print("JSON");
     print(name);
     print(password);
+    print(reflectie_json);
+    print(leerdoel_json); 
+    print(week_reflectie_json);
 
     if(await api.syncUp(name, password, reflectie_json, leerdoel_json, week_reflectie_json)){
       return true;
@@ -28,15 +32,15 @@ class Syncronisation {
   }
 
   static Future<bool> login(String user_name, String password) async{
-    final prefs = await SharedPreferences.getInstance();
-    String? user = prefs.getString('user');
-    if(user == null){
+    // final prefs = await SharedPreferences.getInstance();
+    // String? user = prefs.getString('user');
+    // if(user == null){
       
-      prefs.setString('user', user_name);
-      prefs.setString('password', password);
-      print("user $user_name logged in");
-      return true;
-    }
+    //   prefs.setString('user', user_name);
+    //   prefs.setString('password', password);
+    //   print("user $user_name logged in");
+    //   return true;
+    // }
     return false;
   }
 }
