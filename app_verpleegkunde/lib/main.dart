601a199f44b2336ac55f8c3ad483a7d2aee7e355
MXoneDay <markmolenvd@gmail.com>
2022-02-04 12:02:40 +0100

    refactor main.dart

--- app_verpleegkunde/lib/main.dart
+++ app_verpleegkunde/lib/main.dart
@@ -2,18 +2,18 @@ import 'package:flutter/material.dart';
 import './screens/login/login.dart';
 import 'app_colors.dart';
 
-
-void main() => runApp(HanzeVerpleegkundeApp());
+void main() => runApp(const HanzeVerpleegkundeApp());
 
 class HanzeVerpleegkundeApp extends StatelessWidget {
+  const HanzeVerpleegkundeApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Hanze Verpleegkunde',
       theme: ThemeData(
-          primaryColor: themeColor,
-          scaffoldBackgroundColor: backgroundColor),
+          primaryColor: themeColor, scaffoldBackgroundColor: backgroundColor),
       home: const loginScreen(),
     );
   }
